a38a622e69866e1529a76fcde36ffaaadbde0192
Yiyao Yu <yuydevel@protonmail.com>
2021-03-03 16:55:07 -0500

    Modified to use Q&A model

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use rust_bert::pipelines::conversation::{ConversationManager, ConversationModel};
+use rust_bert::pipelines::question_answering::{QaInput, QuestionAnsweringModel};
 
 mod lib;
 
+const CONFIDENCE_THRESHOLD: f64 = 0.75;
+
 fn start_repl() {
     //TODO Replace default with custom trained model
-    let conv_model = ConversationModel::new(Default::default()).unwrap();
-    let mut conv_manager = ConversationManager::new();
+    let qa_model = QuestionAnsweringModel::new(Default::default()).unwrap();
+    //TODO Add context
     let mut buf = String::new();
+    let context_input = String::from("My name is Ted.");
 
     lib::print_ted_line("Hi, I'm Ted. How may I help you today?");
     loop {
@@ -16,14 +19,13 @@ fn start_repl() {
         match ln.trim() {
             "(over)" | "(o)" | "." => {
                 let trimmed = buf.trim();
-                conv_manager.create(&trimmed);
-                match conv_model
-                    .generate_responses(&mut conv_manager)
-                    .values()
-                    .next()
-                {
-                    Some(response) => lib::print_ted_line(&response),
-                    None => lib::print_ted_line("I didn't quite get that, can you say it again?"),
+                let question = String::from(trimmed);
+                let context = context_input.clone();
+                let ans = &qa_model.predict(&vec![QaInput{ question, context }], 1, 32)[0];
+                if ans.len() > 0 && ans[0].score > CONFIDENCE_THRESHOLD {
+                    lib::print_ted_line(&ans[0].answer)
+                } else {
+                    lib::print_ted_line("I don't have a good answer to that question. Wanna ask something else?")
                 }
                 buf = String::new()
             }
@@ -31,14 +33,13 @@ fn start_repl() {
                 let trimmed = buf.trim();
                 // The next line should be the replaced with model interaction
                 if trimmed.len() != 0 {
-                    conv_manager.create(&trimmed);
-                    match conv_model
-                        .generate_responses(&mut conv_manager)
-                        .values()
-                        .next()
-                    {
-                        Some(response) => lib::print_ted_line(&response),
-                        None => lib::print_ted_line("I didn't quite get that, but bye!"),
+                    let question = String::from(trimmed);
+                    let context = context_input.clone();
+                    let ans = &qa_model.predict(&vec![QaInput{ question, context }], 1, 32)[0];
+                    if ans.len() > 0 && ans[0].score > CONFIDENCE_THRESHOLD {
+                        lib::print_ted_line(&ans[0].answer)
+                    } else {
+                        lib::print_ted_line("I don't have a good answer to that question. But bye!")
                     }
                 } else {
                     lib::print_ted_line("Bye!");
