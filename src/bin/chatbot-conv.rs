4ec080e643c9f2cb4da3d6195e6f14024ff334f9
Paul Ouellette <pouellet@u.rochester.edu>
2021-03-07 13:49:32 -0500

    chatbot-conv: Load model as a TextGenerationModel

--- src/bin/chatbot-conv.rs
+++ src/bin/chatbot-conv.rs
@@ -1,36 +1,39 @@
-use rust_bert::pipelines::conversation::{
-    ConversationConfig, ConversationManager, ConversationModel,
-};
+use rust_bert::pipelines::text_generation::{TextGenerationConfig, TextGenerationModel};
 use rust_bert::resources::{LocalResource, Resource};
 use std::env;
 use std::path::PathBuf;
 
 fn start_repl(model_path: &str) {
-    let config = ConversationConfig {
+    let config = TextGenerationConfig {
         model_resource: Resource::Local(LocalResource {
             local_path: PathBuf::from(model_path),
         }),
-        /* config_resource: Resource::Remote(RemoteResource::from_pretrained(
-            Gpt2ConfigResources::DIALOGPT_MEDIUM,
+        max_length: 30,
+        /*
+        model_type: ModelType::GPT2,
+        model_resource: Resource::Remote(RemoteResource::from_pretrained(
+            Gpt2ModelResources::GPT2_MEDIUM,
+        )),
+        config_resource: Resource::Remote(RemoteResource::from_pretrained(
+            Gpt2ConfigResources::GPT2_MEDIUM,
         )),
         vocab_resource: Resource::Remote(RemoteResource::from_pretrained(
-            Gpt2VocabResources::DIALOGPT_MEDIUM,
+            Gpt2VocabResources::GPT2_MEDIUM,
         )),
         merges_resource: Resource::Remote(RemoteResource::from_pretrained(
-            Gpt2MergesResources::DIALOGPT_MEDIUM,
+            Gpt2MergesResources::GPT2_MEDIUM,
         )),
         min_length: 0,
-        max_length: 1000,
-        min_length_for_response: 32,
+        max_length: 20,
         do_sample: true,
-        early_stopping: false,
-        num_beams: 1,
+        early_stopping: true,
+        num_beams: 5,
         temperature: 1.0,
-        top_k: 50,
+        top_k: 0,
         top_p: 0.9,
         repetition_penalty: 1.0,
         length_penalty: 1.0,
-        no_repeat_ngram_size: 0,
+        no_repeat_ngram_size: 3,
         num_return_sequences: 1,
         num_beam_groups: None,
         diversity_penalty: None,
@@ -39,9 +42,7 @@ fn start_repl(model_path: &str) {
         ..Default::default()
     };
 
-    let conv_model = ConversationModel::new(config).unwrap();
-    let mut conv_manager = ConversationManager::new();
-    let conv_id = conv_manager.create_empty();
+    let model = TextGenerationModel::new(config).unwrap();
     let mut buf = String::new();
 
     chatbot::print_ted_line("Hi, I'm Ted. How may I help you today?");
@@ -52,30 +53,22 @@ fn start_repl(model_path: &str) {
         match ln.trim() {
             "(over)" | "(o)" | "." => {
                 let trimmed = buf.trim();
-                let _ = conv_manager.get(&conv_id).unwrap().add_user_input(&trimmed);
-                match conv_model
-                    .generate_responses(&mut conv_manager)
-                    .values()
-                    .next()
-                {
-                    Some(response) => chatbot::print_ted_line(&response),
-                    None => {
-                        chatbot::print_ted_line("I didn't quite get that, can you say it again?")
-                    }
+                let responses = model.generate(&[trimmed], None);
+                if responses.len() > 0 {
+                    chatbot::print_ted_line(&responses[0]);
+                } else {
+                    chatbot::print_ted_line("I didn't quite get that, can you say it again?");
                 }
                 buf = String::new()
             }
             "(over and out)" | "(oo)" | "bye" => {
                 let trimmed = buf.trim();
                 if trimmed.len() != 0 {
-                    let _ = conv_manager.get(&conv_id).unwrap().add_user_input(&trimmed);
-                    match conv_model
-                        .generate_responses(&mut conv_manager)
-                        .values()
-                        .next()
-                    {
-                        Some(response) => chatbot::print_ted_line(&response),
-                        None => chatbot::print_ted_line("I didn't quite get that, but bye!"),
+                    let responses = model.generate(&[trimmed], None);
+                    if responses.len() > 0 {
+                        chatbot::print_ted_line(&responses[0]);
+                    } else {
+                        chatbot::print_ted_line("I didn't quite get that, but bye!");
                     }
                 } else {
                     chatbot::print_ted_line("Bye!");
